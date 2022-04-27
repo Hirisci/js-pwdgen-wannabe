1258a0984c42c2f7043c820ad064fc905e32d9b8
Hirisci <www.alan92@icloud.com>
2022-04-27 13:34:49 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Chiedi all’utente il suo nome,
+const names= prompt("Inserisci il tuo nome");
+// poi chiedi il suo cognome,
+const surname= prompt("Inserisci il tuo cognome");
+// poi chiedi il suo colore preferito
+const color= prompt("Inserisci il tuo colore preferito");
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+const psw= `${names}${surname}${color}21`;
+console.log(psw)
